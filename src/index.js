5d89b734ce788dd0e463ebd64672660265932224
hillary <korirhillary67@gmail.com>
2023-11-11 11:02:19 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
-
+//ReactDom render
 ReactDOM.render(
   <Form />,
   document.getElementById('root')
